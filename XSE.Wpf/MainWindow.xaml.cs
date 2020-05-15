ac5150b2b8cf461a42d1757f9cc99b00de1fb1c4
Gabriel <tetradog@gmail.com>
2020-05-15 03:48:08 +0200

    desarrollando interficie y compilador XSE

--- XSE.Wpf/MainWindow.xaml.cs
+++ XSE.Wpf/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -21,7 +22,7 @@ namespace XSE.Wpf
     public partial class MainWindow : Window
     {
         public const string PathNotas = "notas.rtb";
-
+        int idTab = 1;
         Timer timerGuardar;
         public MainWindow()
         {
@@ -29,8 +30,10 @@ namespace XSE.Wpf
             if (File.Exists(PathNotas))
                 rtbNotas.AppendText(File.ReadAllText(PathNotas));
             timerGuardar = new Timer();
-            timerGuardar.Interval = 500;
+            timerGuardar.Interval = 100;
             timerGuardar.Elapsed += (s, e) => SaveNotas();
+            tabScripts.SelectionChanged += (s, e) => (tabScripts.SelectedContent as ZonaScript).Refresh();
+            MenuItemNuevaPestañaClick();
         }
 
         void SaveNotas()
@@ -46,6 +49,29 @@ namespace XSE.Wpf
         {
             if (!timerGuardar.Enabled)
                 timerGuardar.Start();
+        }
+
+        private void MenuItemNuevaPestañaClick(object sender=null, RoutedEventArgs e=null)
+        {
+            ContextMenu context = new ContextMenu();
+            MenuItem itemClose = new MenuItem();
+            TabItem pestaña = new TabItem();
+            pestaña.Content = new ZonaScript();
+            tabScripts.Items.Add(pestaña);
+            pestaña.IsSelected = true;
+            pestaña.Header = $"Script{idTab++}";
+            context.Items.Add(itemClose);
+            itemClose.Header = "Close "+pestaña.Header;
+            itemClose.Tag = pestaña;
+            itemClose.Click += (s, m) =>
+            {
+                MenuItem menuItem = ((MenuItem)s);
+                ZonaScript zona = ((TabItem)menuItem.Tag).Content as ZonaScript;
+                if(zona.CanClearContentWithOutProblems())
+                   tabScripts.Items.Remove(menuItem.Tag);
+            };
+            pestaña.ContextMenu = context;
+
         }
     }
 }
--- XSE.Wpf/ZonaScript.xaml.cs
+++ XSE.Wpf/ZonaScript.xaml.cs
@@ -1,8 +1,11 @@
-﻿using Gabriel.Cat.S.Utilitats;
+﻿using Gabriel.Cat.S.Extension;
+using Gabriel.Cat.S.Utilitats;
 using Microsoft.Win32;
 using PokemonGBAFramework.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,40 +24,104 @@ namespace XSE.Wpf
     /// </summary>
     public partial class ZonaScript : UserControl
     {
+        class RomItem:IComparable
+        {
+            public string Path { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                RomItem other = obj as RomItem;
+                int compareTo = other != null ? 0 : -1;
+               
+                if(compareTo==0)
+                   compareTo= Path.CompareTo(other.Path);
+                return compareTo;
+            }
+            public override bool Equals(object obj)
+            {
+                return CompareTo(obj)==0;
+            }
+
+            public override string ToString()
+            {
+                return System.IO.Path.GetFileName(Path);
+            }
+        }
+
+        static List<RomItem> lstRoms;
+        static ZonaScript()
+        {
+            lstRoms = new List<RomItem>();
+            lstRoms.Add(new RomItem() { Path = "" });
+        }
         public ZonaScript()
         {
             InitializeComponent();
+
+            cmbRomsCargadas.ItemsSource = lstRoms;
+            cmbRomsCargadas.SelectionChanged += ChangeRom;
+            
         }
         public RomGba Rom { get; set; }
+        private void ChangeRom(object sender, SelectionChangedEventArgs e)
+        {
+            RomItem romItem;
+            if (cmbRomsCargadas.SelectedIndex > 0)
+            {
+                romItem = ((RomItem)cmbRomsCargadas.SelectedItem);
+                Rom = new RomGba(romItem.Path);
+
+            }
+
+            btnCheck.IsEnabled = Rom != default;
+            btnCompilar.IsEnabled = Rom != default;
+            btnGuardar.IsEnabled = Rom != default;
+            btnReadScript.IsEnabled = Rom != default;
+        }
 
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btnAbrir_Click(object sender, RoutedEventArgs e)
+        private void btnAbrir_Click(object sender=null, RoutedEventArgs e=null)
         {
+            RomItem romItem;
+
             OpenFileDialog opnFile = new OpenFileDialog();
-            opnFile.Filter = "GBA|*.gba";
-            if (opnFile.ShowDialog().GetValueOrDefault())
+            opnFile.Filter = "GBA|*.gba;*.bak";
+            if ( opnFile.ShowDialog().GetValueOrDefault())
             {
                 try
                 {
-                    Rom = new RomGba(opnFile.FileName);
-                    if(cmbRomsCargadas.Items.IndexOf(opnFile.FileName)<0)
-                       cmbRomsCargadas.Items.Add(opnFile.FileName);
-                    cmbRomsCargadas.SelectedIndex = cmbRomsCargadas.Items.IndexOf(opnFile.FileName);
+                    romItem = new RomItem() { Path =  opnFile.FileName};
+                    if (lstRoms.IndexOf(romItem)< 0)
+                    {
+                        lstRoms.Add(romItem);
+                        Refresh();
+                    }
+                    cmbRomsCargadas.SelectedIndex = lstRoms.IndexOf(romItem);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    MessageBox.Show("Ha habido un problema al cargar :"+ex.Message);
+                    MessageBox.Show("Ha habido un problema al cargar :" + ex.Message);
                 }
+
             }
+        }
+
+        public void Refresh()
+        {
+            cmbRomsCargadas.SelectionChanged -= ChangeRom;
+            cmbRomsCargadas.ItemsSource = lstRoms.ToArray();//no les gusta compartir...
+            cmbRomsCargadas.SelectionChanged += ChangeRom;
         }
 
         private void btnCompilar_Click(object sender, RoutedEventArgs e)
         {
-
+            IList<Script> scripts = Script.FromXSE(txtScript.Text.Split('\n'));
+            txtOffsetHex.Text = (Hex)scripts.First().SetScript(Rom);
+            LoadScript();
         }
 
         private void btnCheck_Click(object sender, RoutedEventArgs e)
@@ -64,29 +131,38 @@ namespace XSE.Wpf
 
         private void btnReadScript_Click(object sender, RoutedEventArgs e)
         {
-            Script script;
+
             if (string.IsNullOrEmpty(txtOffsetHex.Text) || string.IsNullOrWhiteSpace(txtOffsetHex.Text))
             {
                 MessageBox.Show("Se requiere un offset para leer el script");
             }
-            else
+            else if(CanClearContentWithOutProblems())
             {
-                ComprobarSiSeHaGuardadoElScript();
-                //try
-                //{
-                    script= new Script(Rom, (int)(Hex)txtOffsetHex.Text);
-                    txtScript.Text = script.GetAllDeclaracionXSE(Rom,null);
-                //}
-                //catch (Exception ex)
-                //{
-                //    MessageBox.Show(ex.Message);
-                //}
+                 LoadScript();
+
             }
         }
 
-        private void ComprobarSiSeHaGuardadoElScript()
+        private Script LoadScript()
         {
+            Script script = new Script(Rom, (int)(Hex)txtOffsetHex.Text);
+            txtScript.Text = script.GetAllDeclaracionXSE(Rom, null);
+            return script;
+        }
 
+        public bool ComprobarSiSeHaGuardadoElScript()
+        {
+            return true;
+        }
+        public bool CanClearContentWithOutProblems()
+        {
+            bool cancontinue = ComprobarSiSeHaGuardadoElScript();
+            if (!cancontinue)
+            {
+                cancontinue = MessageBox.Show("¿Hay datos sin guardar, desas continuar igualmente?", "Atención", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes;
+            }
+            return cancontinue;
         }
     }
+    
 }
